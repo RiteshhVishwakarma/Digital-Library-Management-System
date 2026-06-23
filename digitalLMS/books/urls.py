b1d1eab9747af69603869d0e947b0233387e72d3
Ritesh Vishwakarma <113338495+RiteshhVishwakarma@users.noreply.github.com>
2026-06-23 18:47:00 +0530

    Implement book detail page

--- digitalLMS/books/urls.py
+++ digitalLMS/books/urls.py
@@ -5,5 +5,5 @@ app_name = 'books'
 
 urlpatterns = [
     path('', views.book_list, name='book_list'),
-    path('<int:book_id>/', views.book_detail, name='book_detail'),
+    path('<int:pk>/', views.book_detail, name='book_detail'),
 ]
--- digitalLMS/books/views.py
+++ digitalLMS/books/views.py
@@ -1,15 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
 
 def book_list(request):
     """
     Display a list of all books.
     """
-    pass
+    books = Book.objects.all()
+    context = {
+        'books': books,
+        'total_books': books.count()
+    }
+    return render(request, 'books/book_list.html', context)
 
 
-def book_detail(request, book_id):
+def book_detail(request, pk):
     """
     Display details of a specific book.
     """
-    pass
+    book = get_object_or_404(Book, pk=pk)
+    context = {
+        'book': book
+    }
+    return render(request, 'books/book_detail.html', context)
